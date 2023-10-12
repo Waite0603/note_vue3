c3ad7600cded05964d8988e37e05e944082a1b54
waite0603 <waite@waite.wang>
2023-10-13 00:03:28 +0800

    Development: Introduction of Vant on demand

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// vant auto import
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    Components({
+      resolvers: [VantResolver()],
+    })
   ],
   resolve: {
     alias: {
